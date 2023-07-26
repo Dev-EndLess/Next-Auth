fb19e34d05d9077e83031f5c005cce565e6217ab
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2023-07-26 15:16:25 +0200

    delete useless file

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className="bg-yellow-400">
-      <h1 className="text-slate-900">This is the Admin Page</h1>
-    </div>
-  )
-}
